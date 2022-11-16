f11420cd62f3937add33048d541ca92447e87e63
cbj252 <cbj252@gmail.com>
2022-11-16 21:13:37 +0800

    Backup for dependancy update

--- config/mongoConfig.js
+++ config/mongoConfig.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const mongoDb = process.env.DB_URL;
-
 mongoose.connect(mongoDb, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
