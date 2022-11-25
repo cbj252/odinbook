75adde0292b38ebb1b0e8d073efaacf61605593d
cbj252 <cbj252@gmail.com>
2022-11-25 21:29:59 +0800

    Changed comment text box to move into view of user

--- public/commentBox.js
+++ public/commentBox.js
@@ -1,15 +1,23 @@
 // Needs to be done here as this is cilent-side.
 
+function isVisible(e) {
+  return !!(e.offsetWidth || e.offsetHeight || e.getClientRects().length);
+}
+
 const allCommentButtons = document.querySelectorAll(".commentButton");
-allCommentButtons.forEach(function(button) {
+allCommentButtons.forEach(function (button) {
   const buttonId = button.id;
   const areaId = buttonId.slice(0, -6);
   const associatedInputArea = document.getElementById(areaId);
-  button.addEventListener("click", function() {
+  button.addEventListener("click", function () {
     if (associatedInputArea.style.display == "none") {
       associatedInputArea.style.display = "flex";
+      // 200 is arbitary - anything more than the width of one comment box is fine.
+      document.documentElement.scrollTop =
+        document.documentElement.scrollTop + 200;
+      document.body.scrollTop = document.body.scrollTop + 200;
     } else {
       associatedInputArea.style.display = "none";
     }
-  })
-})
+  });
+});
